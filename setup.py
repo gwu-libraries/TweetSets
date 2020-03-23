4d4b85656016ce235e3e2f1decc05c28646885c7
Laura Wrubel <lwrubel@gwu.edu>
2020-03-23 14:12:02 -0400

    fixes #37. Updates Tweetsets version in setup.py.

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         name='TweetSets',
-        version='0.1',
-        url='https://github.com/justinlittman/TweetSets',
-        author='Justin Littman',
-        author_email='justinlittman@gmail.com',
+        version='1.1.1',
+        url='https://github.com/gwu-libraries/TweetSets',
+        author='GWU Libraries',
+        author_email='sfm@gwu.edu',
         scripts=['tweetset_cli.py'],
         description='Archive tweets from the command line',
         install_requires=open('requirements.txt').read().split(),
