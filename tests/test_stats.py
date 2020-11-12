802c5e531fcd0d6bbedb932904fce241496cc396
Dosly Smith <dsmith@gwu.edu>
2020-11-12 16:06:01 -0500

    Updated tests for stats

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -2,16 +2,20 @@ from unittest import TestCase
 import tempfile
 import os
 import shutil
-from stats import TweetSetStats
-from datetime import datetime
+from stats import TweetSetStats, SourceDatasetMergeStat, DerivativeMergeStat
+from datetime import datetime, timedelta
+import random
 from time import sleep
+from collections import Counter
 
+NUM_ROWS = 100
 
 class TestTweetSetStats(TestCase):
 
     def setUp(self):
         self.db_path = tempfile.mkdtemp()
         self.stats = TweetSetStats(db_filepath=os.path.join(self.db_path, 'stat.db'))
+        self.since = datetime.utcnow() - timedelta(days=30 * 6)
 
     def tearDown(self):
         shutil.rmtree(self.db_path, ignore_errors=True)
@@ -33,13 +37,16 @@ class TestTweetSetStats(TestCase):
         self.assertEqual((0, None), self.stats.datasets_stats(since_datetime=after))
 
     def test_source_datasets(self):
-        self.stats.add_source_dataset('b', True)
-        self.stats.add_source_dataset('a', True)
-        self.stats.add_source_dataset('a', False)
-
-        self.assertEqual([('a', 2), ('b', 1)], self.stats.source_datasets_stats())
-        self.assertEqual([('a', 2)], self.stats.source_datasets_stats(limit=1))
-        self.assertEqual([('a', 1), ('b', 1)], self.stats.source_datasets_stats(local_only=True))
+        dataset_ids = [random.choice('ABCD') for i in range(NUM_ROWS)]
+        is_local = [random.choice([1, 0]) for i in range(NUM_ROWS)]
+        source_stats = Counter()
+        for d, l in zip(dataset_ids, is_local):
+            self.stats.add_source_dataset(d, l)
+            source_stats[d] += 1
+        stats = self.stats.source_datasets_stats(self.since, limit=NUM_ROWS)
+        for s in stats:
+            self.assertEqual(len([d for d in dataset_ids if d == s.dataset_id]), s.all_count)
+            self.assertEqual(source_stats[s.dataset_id], s.local_count)
 
     def test_derivatives(self):
         self.stats.add_derivative('b', True)
